b22dbea9283014e64aa6573e2275db140d485019
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2023-10-04 20:11:05 +0330

    ui improved

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color.fromARGB(255, 221, 34, 34);
-const Color tdBlue = Color.fromARGB(255, 49, 108, 226);
+const Color tdRed = Color.fromARGB(255, 239, 17, 17);
+const Color tdBlue = Color.fromARGB(255, 16, 90, 237);
 
 const Color tdBlack = Color.fromARGB(255, 0, 0, 0);
 const Color tdGrey = Color.fromARGB(255, 163, 163, 163);
